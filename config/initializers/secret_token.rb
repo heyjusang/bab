c40bcdc97bebabf00a3ebb72f66730125af39db8
co9901 <co9901@co9901.cafe24.com>
2011-12-19 15:44:00 +0900

    testgit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bab::Application.config.secret_token = '3604365151ac4e63a82f6d211f03931e9651aacd780a4f0fec435903d28684e562d76c2231d539a192467905ec865aa4e8214066255eb6478189e2ebd20d4c36'
